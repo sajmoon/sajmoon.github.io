b9ac267b8166be3e641f91fc6d4ba11c8b01abb6
Simon Strom <simon.strom@gmail.com>
2018-01-07 21:25:49 +0100

    Updates

---
+++ path---hello-world-dfebb41656afdd4d12bf.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x834755aae49e],{477:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Tinkering with Code.",author:"Simon Ström"}},markdownRemark:{id:"/Users/simon/Code/sajmoon/src/pages/hello-world/index.md absPath of file >>> MarkdownRemark",html:"<p>Started a gatsby blog. Will migrate old blog posts here later.</p>",frontmatter:{title:"Installed Gatsby",date:"January 07, 2017"}}},pathContext:{slug:"/hello-world/"}}}});
+//# sourceMappingURL=path---hello-world-dfebb41656afdd4d12bf.js.map
