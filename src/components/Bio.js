dc6a86e1227b608af5ec21ed27693858f3b77841
Simon Strom <simon.strom@gmail.com>
2018-01-07 20:56:47 +0100

    Initial commit

---
+++ src/components/Bio.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Simon Ström`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          Written by <strong>Simon Ström</strong> as a way to remember. It's a
+          dev log of thinks I want to remember.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/pages/hello-world/index.md
@@ -0,0 +1,6 @@
+---
+title: Installed Gatsby
+date: "2017-01-07T20:52:03.284Z"
+---
+
+Started a gatsby blog. Will migrate old blog posts here later.
