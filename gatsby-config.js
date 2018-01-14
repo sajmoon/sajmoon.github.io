f1ca3a7ef51bbf8d4182f0959ba1dcb0a7064e13
Simon Strom <simon.strom@gmail.com>
2018-01-14 22:29:00 +0100

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-45217361-8',
       },
     },
     `gatsby-plugin-feed`,
