9757e2c7583a749f826b89a9bd20e97d85405c49
Simon Strom <simon.strom@gmail.com>
2018-01-21 21:23:16 +0100

    A 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Nope, nothing here.</h1>
+
+        <p>
+          You really should go back and see if you can find your content
+          somewhere else.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default NotFound
