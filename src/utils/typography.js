6c086afa25c12797986901ac774616816008b155
Simon Strom <simon.strom@gmail.com>
2018-01-21 23:16:06 +0100

    Change typeface

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,13 +1,7 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import moragaTheme from 'typography-theme-moraga'
 
-Wordpress2016.overrideThemeStyles = () => ({
-  'a.gatsby-resp-image-link': {
-    boxShadow: 'none',
-  },
-})
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(moragaTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
