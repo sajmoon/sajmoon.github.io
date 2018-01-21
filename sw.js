8584e11b23595c3ce58aa77b44a1e071e7d47e24
Simon Strom <simon.strom@gmail.com>
2018-01-21 21:24:06 +0100

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-1b77efc49e1969d44f2e.js","1c63d2abf7e9f3b72d62116db2d26e60"],["/app-3b33baefff212b7887f3.js","3a43715fc2a00c7597b248f49955b624"],["/app-777e5182795cee48a52c.js","64ac6d30186a9c3c7e9eed5e34d80920"],["/app-b61350173b9a36fc1282.js","f729babf312a391eca9102bc25c549a3"],["/app-bf40d4c4e0f7f4f9b770.js","a4c166e5421b7868a6896d1df7b09f49"],["/commons-78727bd33a365b16d4bb.js","92e1a0d3d98d55250e2d33079999c003"],["/index.html","931f5d39839ce24353e004d034d75981"],["/offline-plugin-app-shell-fallback/index.html","cdd7004073c030b5222dcacce3efea06"]];
+var precacheConfig = [["/app-710978858cda401c038a.js","b00a85561851e1cef4a397f6cf17adc7"],["/commons-b4be30d634a1b5fad357.js","2f27147891a9c6c5f426580ea35304ca"],["/index.html","fffa1b4df242d96c825b673ac1324058"],["/offline-plugin-app-shell-fallback/index.html","700283561a72d5caeb54918317294163"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
